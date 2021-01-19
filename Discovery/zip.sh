86ca901deaa4c5905543cee50fdcb38c807afb87
yuchao <yuchao9225@163.com>
2021-01-19 17:17:07 +0800

    add script...

--- Discovery/zip.sh
+++ Discovery/zip.sh
@@ -3,10 +3,19 @@
 # 磁盘文件太大，git传输不上去，git commit 需要压缩Bochs，
 # linux-0.11-devel-060625下文件
 
-rm Bochs.zip linux-0.11-devel-060625-local.zip
+bochs_file="Bochs"
+linux_devel_file="linux-0.11-devel-060625"
 
-zip -r Bochs.zip ./Bochs/*
-zip -r linux-0.11-devel-060625-local.zip ./linux-0.11-devel-060625/*
+if [ -d "${bochs_file}" ]; then
+	echo "zip ${bochs_file}..."
+	zip -r ${bochs_file}.zip ${bochs_file}/*
+	rm ${bochs_file} -rf
+fi
 
-rm ./Bochs ./linux-0.11-devel-060625 -rf
+if [ -d "${linux_devel_file}" ]; then
+	echo "zip ${linux_devel_file}..."
+	zip -r ${linux_devel_file}-local.zip ${linux_devel_file}/*
+	rm ${linux_devel_file} -rf
+fi
+
 
